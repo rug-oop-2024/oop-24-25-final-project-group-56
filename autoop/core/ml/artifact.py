223c6a99f6bdd062b73b657a70406fdebc8c0ae8
Mihai Damian <mihaidamian345@gmail.com>
2024-10-21 13:01:32 +0200

    added feature detect and artifact base class

--- autoop/core/ml/artifact.py
+++ autoop/core/ml/artifact.py
@@ -1,5 +1,22 @@
 from pydantic import BaseModel, Field
+from typing import Dict, List
 import base64
 
+
 class Artifact(BaseModel):
-    pass
+    asset_path: str = Field(..., description="Path to the asset")
+    version: str = Field(..., description="Version of the asset")
+    data: bytes = Field(..., description="Binary state data")
+    metadata: Dict[str, str] = Field(
+        ..., description="Metadata containing experiment and run IDs"
+    )
+    type: str = Field(..., description="Type of model")
+    tags: List[str] = Field(
+        ..., description="Tags associated with the artifact"
+    )
+
+    def generate_id(self) -> str:
+        encoded_path = base64.b64encode(
+            self.asset_path.encode('utf-8')
+        ).decode('utf-8')
+        return f"{encoded_path}:{self.version}"
--- autoop/functional/feature.py
+++ autoop/functional/feature.py
@@ -3,6 +3,7 @@ from typing import List
 from autoop.core.ml.dataset import Dataset
 from autoop.core.ml.feature import Feature
 
+
 def detect_feature_types(dataset: Dataset) -> List[Feature]:
     """Assumption: only categorical and numerical features and no NaN values.
     Args:
@@ -10,4 +11,11 @@ def detect_feature_types(dataset: Dataset) -> List[Feature]:
     Returns:
         List[Feature]: List of features with their types.
     """
-    raise NotImplementedError("This should be implemented by you.")
+    features = []
+    for row in dataset.rows:
+        for column in dataset.columns:
+            if isinstance(dataset[row][column], str):
+                features.append(Feature(name=column, type='categorical'))
+            else:
+                features.append(Feature(name=column, type='numerical'))
+    return features
